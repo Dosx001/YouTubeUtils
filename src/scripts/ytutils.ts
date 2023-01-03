cef7b39e4964ec0eee3e3e97f81cb2de4cc54c2b
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-03 12:40:49 -0800

    Rewrote changeVideoSize

--- src/scripts/ytutils.ts
+++ src/scripts/ytutils.ts
@@ -24,7 +24,7 @@ const ytutils = {
   player: document.querySelector<player>("#movie_player"),
   requestChange: () => {
     ytutils.changeVideoQuality();
-    ytutils.changeVideoSize(ytutils.isOptionHandle);
+    ytutils.changeVideoSize();
     ytutils.expandVideoDescription(ytutils.isOptionHandle);
     ytutils.enablesuggestedautoplay();
   },
@@ -239,398 +239,53 @@ const ytutils = {
       }
     }
   },
-  doTheRest: (size: HTMLElement, watch: HTMLElement) => {
-    if (
-      document.getElementById("masthead-container") &&
-      document.getElementById("page-manager")
-    ) {
-      ytutils.scrollTo();
-      const interval = document.defaultView.setInterval(() => {
-        if (size?.getAttribute("d").indexOf("m 28,") === 0 || !watch) {
-          if (size.parentNode.parentNode) {
-            (size.parentNode.parentNode as HTMLElement).click();
-            document.defaultView.clearInterval(interval);
-          }
-        } else {
-          document.defaultView.clearInterval(interval);
-        }
-      }, 1000);
-    }
-  },
-  changeVideoSize: (isOptionHandle) => {
+  changeVideoSize: () => {
     if (
       document.getElementById("playnav-player") ||
       document.location.pathname.search(/^\/watch/) !== 0
     )
       return;
-    const player = document.querySelector("#player");
-    const container = document.querySelector(".html5-video-container");
-    const api = document.querySelector<HTMLElement>("#player-api");
-    const size = document.querySelector<HTMLElement>(".ytp-size-button path");
-    const watch = document.querySelector<HTMLElement>("ytd-watch[theater]");
-    const appbar = document.querySelector<HTMLElement>(
-      "#watch-appbar-playlist"
-    );
-    if (ytutils.size === "fullpage") {
-      if (player) {
-        player.removeAttribute("style");
-      }
-      if (container) {
-        container.removeAttribute("style");
-      }
-      if (api) {
-        api.style.removeProperty("margin-left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("margin-top");
-      }
-
-      if ((size && size.getAttribute("d").indexOf("m 28,") == 0) || !watch) {
-        if (
-          size &&
-          size.getAttribute("d").indexOf("m 28,") == 0 &&
-          size.parentNode.parentNode
-        ) {
-          size.parentNode.parentNode.click();
-        }
-      }
-
-      if (
-        document.getElementById("watch7-container") &&
-        document
-          .getElementById("watch7-container")
-          .classList.contains("watch-wide")
-      ) {
-        document
-          .getElementById("watch7-container")
-          .classList.remove("watch-wide");
-      }
-      if (player && player.classList.contains("watch-playlist-collapsed")) {
-        player.classList.remove("watch-playlist-collapsed");
-      }
-      if (player && player.classList.contains("watch-medium")) {
-        player.classList.remove("watch-medium");
-      }
-      if (player && player.classList.contains("watch-small")) {
-        player.classList.remove("watch-small");
-      }
-      if (
-        document.getElementById("page") &&
-        document
-          .getElementById("page")
-          .classList.contains("watch-non-stage-mode")
-      ) {
-        document
-          .getElementById("page")
-          .classList.remove("watch-non-stage-mode");
-      }
-      if (document.getElementById("page")) {
-        document.getElementById("page").classList.add("watch-stage-mode");
-      }
-      if (document.getElementById("page")) {
-        document.getElementById("page").classList.add("watch-wide");
-      }
-      var interwal;
-      var it = 0;
-
-      if (document.querySelector("#player.ytd-watch")) {
-        document
-          .querySelector("#player.ytd-watch")
-          .setAttribute(
-            "style",
-            "height:" + document.defaultView.innerHeight + "px !important"
-          );
-      } else {
-        interwal = document.defaultView.setInterval(function() {
-          if (!document.querySelector("#player.ytd-watch")) {
-            it++;
-            return;
-          } else {
-            document.defaultView.clearInterval(interwal);
+    switch (ytutils.size) {
+      case "expand": {
+        const id = setInterval(() => {
+          const flexy = document.querySelector("ytd-watch-flexy");
+          if (flexy) {
+            flexy.theaterModeChanged_(true);
+            clearInterval(id);
           }
-          document
-            .querySelector("#player.ytd-watch")
-            .setAttribute(
-              "style",
-              "height:" + document.defaultView.innerHeight + "px !important"
-            );
-          ytutils.doTheRest(size);
+        }, 100);
+        break;
+      }
+      // case "chat": {
+      //   const outer = setInterval(() => {
+      //     const flexy = document.querySelector("ytd-watch-flexy");
+      //     if (flexy) {
+      //       clearInterval(outer);
+      //       let conut = 0;
+      //       const inner = setInterval(() => {
+      //         if (document.querySelector("#chat")) {
+      //           flexy.theaterModeChanged_(false);
+      //           clearInterval(inner);
+      //         }
+      //         if (conut++ === 20) {
+      //           flexy.theaterModeChanged_(true);
+      //           clearInterval(inner);
+      //         }
+      //       }, 100);
+      //     }
+      //   }, 100);
+      //   break;
+      // }
+      default: {
+        const id = setInterval(() => {
+          const flexy = document.querySelector("ytd-watch-flexy");
+          if (flexy) {
+            flexy.theaterModeChanged_(false);
+            clearInterval(id);
+          }
         }, 100);
       }
-
-      if (container && ytutils.player) {
-        container.setAttribute(
-          "style",
-          "width:" +
-          document.body.clientWidth +
-          "px !important;margin-left:-" +
-          ytutils.player.getBoundingClientRect().x +
-          "px !important;"
-        );
-      }
-
-      if (document.getElementById("placeholder-playlist") && appbar) {
-        var element = document.getElementById("placeholder-playlist");
-        var rect = element.getBoundingClientRect();
-        var element2 = appbar;
-        var rect2 = element2.getBoundingClientRect();
-        if (rect.y != rect2.y) {
-          if (appbar) {
-            appbar.style.setProperty(
-              "margin-top",
-              rect.y - rect2.y + "px",
-              "important"
-            );
-          }
-        }
-      }
-      ytutils.doTheRest(size);
-    } else if (ytutils.size === "expand") {
-      if ((size && size.getAttribute("d").indexOf("m 28,") == 0) || !watch) {
-        if (
-          size &&
-          size.getAttribute("d").indexOf("m 28,") == 0 &&
-          size.parentNode.parentNode
-        ) {
-          size.parentNode.parentNode.click();
-        }
-      }
-
-      if (document.querySelector("#player.ytd-watch")) {
-        document.querySelector("#player.ytd-watch").removeAttribute("style");
-      }
-      if (container) {
-        container.removeAttribute("style");
-      }
-      if (api) {
-        api.style.removeProperty("margin-left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("margin-top");
-      }
-
-      if (document.getElementById("watch7-container")) {
-        document.getElementById("watch7-container").classList.add("watch-wide");
-      }
-      if (player) {
-        player.classList.add("watch-playlist-collapsed");
-      }
-      if (player) {
-        player.classList.add("watch-medium");
-      }
-      if (document.getElementById("page")) {
-        document.getElementById("page").classList.add("watch-stage-mode");
-      }
-      if (document.getElementById("page")) {
-        document.getElementById("page").classList.add("watch-wide");
-      }
-      if (player && player.classList.contains("watch-small")) {
-        player.classList.remove("watch-small");
-      }
-      if (
-        document.getElementById("page") &&
-        document
-          .getElementById("page")
-          .classList.contains("watch-non-stage-mode")
-      ) {
-        document
-          .getElementById("page")
-          .classList.remove("watch-non-stage-mode");
-      }
-
-      if (isOptionHandle) {
-        document.defaultView.scrollTo(0, 0);
-      }
-
-      if (document.getElementsByClassName("html5-main-video")[0]) {
-        document.getElementsByClassName("html5-main-video")[0].style.top =
-          "0px";
-      }
-
-      var interwal = document.defaultView.setInterval(function() {
-        if ((size && size.getAttribute("d").indexOf("m 28,") == 0) || !watch) {
-          if (
-            size &&
-            size.getAttribute("d").indexOf("m 28,") == 0 &&
-            size.parentNode.parentNode
-          ) {
-            size.parentNode.parentNode.click();
-            document.defaultView.clearInterval(interwal);
-          }
-        } else {
-          document.defaultView.clearInterval(interwal);
-        }
-      }, 1000);
-    } else if (ytutils.size === "shrink") {
-      if (document.querySelector("#player.ytd-watch")) {
-        document.querySelector("#player.ytd-watch").removeAttribute("style");
-      }
-      if (container) {
-        container.removeAttribute("style");
-      }
-      if (api) {
-        api.style.removeProperty("margin-left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("left");
-      }
-      if (appbar) {
-        appbar.style.removeProperty("margin-top");
-      }
-
-      if ((size && size.getAttribute("d").indexOf("m 26,") == 0) || watch) {
-        if (
-          size &&
-          size.getAttribute("d").indexOf("m 26,") == 0 &&
-          size.parentNode.parentNode
-        ) {
-          size.parentNode.parentNode.click();
-        }
-      }
-
-      if (
-        document.getElementById("watch7-container") &&
-        document
-          .getElementById("watch7-container")
-          .classList.contains("watch-wide")
-      ) {
-        document
-          .getElementById("watch7-container")
-          .classList.remove("watch-wide");
-      }
-      if (player && player.classList.contains("watch-playlist-collapsed")) {
-        player.classList.remove("watch-playlist-collapsed");
-      }
-      if (player && player.classList.contains("watch-medium")) {
-        player.classList.remove("watch-medium");
-      }
-      if (player) {
-        player.classList.add("watch-small");
-      }
-      if (document.getElementById("page")) {
-        document.getElementById("page").classList.add("watch-non-stage-mode");
-      }
-      if (player && player.classList.contains("watch-wide")) {
-        player.classList.remove("watch-wide");
-      }
-      if (
-        document.getElementById("page") &&
-        document.getElementById("page").classList.contains("watch-stage-mode")
-      ) {
-        document.getElementById("page").classList.remove("watch-stage-mode");
-      }
-      if (
-        document.getElementById("page") &&
-        document.getElementById("page").classList.contains("watch-wide")
-      ) {
-        document.getElementById("page").classList.remove("watch-wide");
-      }
-
-      if (isOptionHandle) {
-        document.defaultView.scrollTo(0, 0);
-      }
-
-      var interwal = document.defaultView.setInterval(function() {
-        if ((size && size.getAttribute("d").indexOf("m 26,") == 0) || watch) {
-          if (
-            size &&
-            size.getAttribute("d").indexOf("m 26,") == 0 &&
-            size.parentNode.parentNode
-          ) {
-            size.parentNode.parentNode.click();
-            document.defaultView.clearInterval(interwal);
-          }
-        } else {
-          document.defaultView.clearInterval(interwal);
-        }
-      }, 1000);
-    } else if (ytutils.size === "default") {
-      if (isOptionHandle) {
-        if (document.querySelector("#player.ytd-watch")) {
-          document.querySelector("#player.ytd-watch").removeAttribute("style");
-        }
-        if (container) {
-          container.removeAttribute("style");
-        }
-        if (api) {
-          api.style.removeProperty("margin-left");
-        }
-        if (appbar) {
-          appbar.style.removeProperty("left");
-        }
-        if (appbar) {
-          appbar.style.removeProperty("margin-top");
-        }
-
-        if ((size && size.getAttribute("d").indexOf("m 26,") == 0) || watch) {
-          if (
-            size &&
-            size.getAttribute("d").indexOf("m 26,") == 0 &&
-            size.parentNode.parentNode
-          ) {
-            size.parentNode.parentNode.click();
-          }
-        }
-
-        if (
-          document.getElementById("watch7-container") &&
-          document
-            .getElementById("watch7-container")
-            .classList.contains("watch-wide")
-        ) {
-          document
-            .getElementById("watch7-container")
-            .classList.remove("watch-wide");
-        }
-        if (player && player.classList.contains("watch-playlist-collapsed")) {
-          player.classList.remove("watch-playlist-collapsed");
-        }
-        if (player && player.classList.contains("watch-medium")) {
-          player.classList.remove("watch-medium");
-        }
-        if (player) {
-          player.classList.add("watch-small");
-        }
-        if (document.getElementById("page")) {
-          document.getElementById("page").classList.add("watch-non-stage-mode");
-        }
-        if (player && player.classList.contains("watch-wide")) {
-          player.classList.remove("watch-wide");
-        }
-        if (
-          document.getElementById("page") &&
-          document.getElementById("page").classList.contains("watch-stage-mode")
-        ) {
-          document.getElementById("page").classList.remove("watch-stage-mode");
-        }
-
-        if (isOptionHandle) {
-          document.defaultView.scrollTo(0, 0);
-        }
-
-        var interwal = document.defaultView.setInterval(function() {
-          if ((size && size.getAttribute("d").indexOf("m 26,") == 0) || watch) {
-            if (
-              size &&
-              size.getAttribute("d").indexOf("m 26,") == 0 &&
-              size.parentNode.parentNode
-            ) {
-              size.parentNode.parentNode.click();
-            }
-          } else {
-            document.defaultView.clearInterval(interwal);
-          }
-        }, 1000);
-      }
     }
-
-    if (document.body) document.body.classList.add("fullytpagesize");
   },
 };
 
