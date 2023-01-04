89a401bcc9ab281fb9070e2b8f40bbb95a4dd7d3
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-04 15:28:05 -0800

    Made common queries global in popup

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,30 +1,36 @@
+const [sel0, sel1, sel2] = document.querySelectorAll("select");
+const volLvl = document.querySelector<HTMLInputElement>("#volumelevel");
+const autoexpand = document.querySelector<HTMLInputElement>(
+  "#autoexpanddescription"
+);
+const embedded = document.querySelector<HTMLSelectElement>(
+  "#embeddedvideoautoplaybehavior"
+);
+const playlist = document.querySelector<HTMLSelectElement>(
+  "#playlistvideoautoplaybehavior"
+);
+const autoplay = document.querySelector<HTMLSelectElement>(
+  "#youtubevideoautoplaybehavior"
+);
+const sugguest = document.querySelector<HTMLInputElement>("#suggestedautoplay");
+
 const updateSettings = async () => {
-  const [sel0, sel1, sel2] = document.querySelectorAll("select");
   browser.storage.sync.set({
-    autoexpanddescription: document.querySelector<HTMLInputElement>(
-      "#autoexpanddescription"
-    ).checked,
+    autoexpanddescription: autoexpand.checked,
     autosubtitles: document.querySelector<HTMLInputElement>(
       '#autosubtitles input[type="radio"][name="autosubtitles"]:checked'
     ).value,
-    embeddedvideoautoplaybehavior: document.querySelector<HTMLInputElement>(
-      "#embeddedvideoautoplaybehavior"
-    ).value,
-    playlistvideoautoplaybehavior: document.querySelector<HTMLInputElement>(
-      "#playlistvideoautoplaybehavior"
-    ).value,
+    embeddedvideoautoplaybehavior: embedded.value,
+    playlistvideoautoplaybehavior: playlist.value,
     quality: sel0.options[sel0.selectedIndex].value,
     size: sel1.options[sel1.selectedIndex].value,
     speed: sel2.options[sel2.selectedIndex].value,
-    suggestedautoplay:
-      document.querySelector<HTMLInputElement>("#suggestedautoplay").checked,
+    suggestedautoplay: sugguest.checked,
     volume: document.querySelector<HTMLInputElement>(
       '#volume input[type="radio"][name="volume"]:checked'
     ).value,
-    volumelevel: document.querySelector<HTMLInputElement>("#volumelevel").value,
-    youtubevideoautoplaybehavior: document.querySelector<HTMLInputElement>(
-      "#youtubevideoautoplaybehavior"
-    ).value,
+    volumelevel: volLvl.value,
+    youtubevideoautoplaybehavior: autoplay.value,
   });
   for (const tab of await browser.tabs.query({
     active: true,
@@ -37,7 +43,6 @@ const updateSettings = async () => {
 };
 
 browser.storage.sync.get((data: settings) => {
-  const [sel0, sel1, sel2] = document.querySelectorAll("select");
   sel0.value = data.quality;
   sel1.value = data.size;
   sel2.value = data.speed;
@@ -47,37 +52,23 @@ browser.storage.sync.get((data: settings) => {
   document.getElementById("volume").onchange = updateSettings;
   document.querySelector<HTMLInputElement>(`.vol_${data.volume}`).checked =
     true;
-  const volLvl = document.querySelector<HTMLInputElement>("#volumelevel");
   volLvl.value = data.volumelevel;
   volLvl.onchange = updateSettings;
   volLvl.onfocus = () =>
     document.querySelector<HTMLInputElement>("#volumelevelinput").click();
-  const sug = document.querySelector<HTMLInputElement>("#suggestedautoplay");
-  sug.checked = data.suggestedautoplay;
-  sug.onchange = updateSettings;
-  const expand = document.querySelector<HTMLInputElement>(
-    "#autoexpanddescription"
-  );
-  expand.checked = data.autoexpanddescription;
-  expand.addEventListener("change", updateSettings, true);
+  sugguest.checked = data.suggestedautoplay;
+  sugguest.onchange = updateSettings;
+  autoexpand.checked = data.autoexpanddescription;
+  autoexpand.addEventListener("change", updateSettings, true);
   document.querySelector<HTMLSelectElement>("#autosubtitles").onchange =
     updateSettings;
   document
     .querySelector<HTMLInputElement>(`.subt_${data.autosubtitles}`)
     .click();
-  const autoplybehr = document.querySelector<HTMLSelectElement>(
-    "#youtubevideoautoplaybehavior"
-  );
-  autoplybehr.onchange = updateSettings;
-  autoplybehr.value = data.youtubevideoautoplaybehavior;
-  const playlist = document.querySelector<HTMLSelectElement>(
-    "#playlistvideoautoplaybehavior"
-  );
+  autoplay.onchange = updateSettings;
+  autoplay.value = data.youtubevideoautoplaybehavior;
   playlist.onchange = updateSettings;
   playlist.value = data.playlistvideoautoplaybehavior;
-  const embedded = document.querySelector<HTMLSelectElement>(
-    "#embeddedvideoautoplaybehavior"
-  );
   embedded.onchange = updateSettings;
   embedded.value = data.embeddedvideoautoplaybehavior;
 });
