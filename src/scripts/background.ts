5342ae90551fe9c83cc7e66b525b9596851b9431
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-06 11:14:38 -0800

    Simplify background script

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,17 +1,16 @@
-const settings = {
-  autoexpanddescription: true,
-  autosubtitles: "default",
-  embeddedvideoautoplaybehavior: "default",
-  playlistvideoautoplaybehavior: "default",
-  quality: "highres",
-  size: "expand",
-  speed: 1,
-  suggestedautoplay: true,
-  volume: "default",
-  volumelevel: 100,
-  youtubevideoautoplaybehavior: "default",
-};
-
-browser.storage.sync.get((data: typeof settings) => {
-  if (Object.keys(data).length === 0) browser.storage.sync.set(settings);
+browser.storage.sync.get((data: settings) => {
+  if (Object.keys(data).length === 0)
+    browser.storage.sync.set({
+      autoexpanddescription: true,
+      autosubtitles: "default",
+      embeddedvideoautoplaybehavior: "default",
+      playlistvideoautoplaybehavior: "default",
+      quality: "highres",
+      size: "expand",
+      speed: 1,
+      suggestedautoplay: true,
+      volume: "default",
+      volumelevel: 100,
+      youtubevideoautoplaybehavior: "default",
+    });
 });
