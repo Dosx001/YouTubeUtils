5061dc85fe8c87e4f4404968713afa086aa783fa
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-09 14:42:43 -0800

    Added loop hotkey and ui

--- src/scripts/ytutils.ts
+++ src/scripts/ytutils.ts
@@ -4,6 +4,7 @@ interface player extends HTMLElement {
   getPlaybackQuality: () => string;
   getPlayerState: () => number;
   setAutonav: (state: boolean) => void;
+  setLoopVideo: (state: boolean) => void;
   setPlaybackQualityRange: (min: string, max: string) => void;
   setPlaybackRate: (rate: number) => void;
   setVolume: (level: number) => void;
@@ -24,6 +25,7 @@ const ytutils = {
   subtitles: "default",
   volume: "default",
   volumelevel: 100,
+  loop: false,
   player: document.querySelector<player>("#movie_player")!,
   updatePlayer: () => {
     ytutils.setQuality();
@@ -61,9 +63,6 @@ const ytutils = {
       default:
         return ytutils.volumelevel;
     }
-  },
-  onSPFDone: () => {
-    window.postMessage({ type: "GET_SETTINGS", text: "NULL" }, "*");
   },
   setQuality: () => {
     if (ytutils.quality !== "default") {
@@ -129,8 +128,18 @@ window.onmessage = (ev: MessageEvent) => {
   }
 };
 
-window.addEventListener("spfdone", ytutils.onSPFDone);
-window.addEventListener("yt-navigate-start", ytutils.onSPFDone);
+const on =
+  "m2.164 10.201c.055-.298.393-.734.934-.59.377.102.612.476.543.86-.077.529-.141.853-.141 1.529 0 4.47 3.601 8.495 8.502 8.495 2.173 0 4.241-.84 5.792-2.284l-1.251-.341c-.399-.107-.636-.519-.53-.919.108-.4.52-.637.919-.53l3.225.864c.399.108.637.519.53.919l-.875 3.241c-.107.399-.519.636-.919.53-.399-.107-.638-.518-.53-.918l.477-1.77c-1.829 1.711-4.27 2.708-6.838 2.708-5.849 0-9.968-4.8-10.002-9.93-.003-.473.027-1.119.164-1.864zm5.396 2.857 2.924 2.503c.142.128.321.19.499.19.202 0 .405-.081.552-.242l4.953-5.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-4.453 4.962-2.371-2.011c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557zm14.276.743c-.054.298-.392.734-.933.59-.378-.102-.614-.476-.543-.86.068-.48.139-.848.139-1.53 0-4.479-3.609-8.495-8.5-8.495-2.173 0-4.241.841-5.794 2.285l1.251.341c.4.107.638.518.531.918-.108.4-.519.637-.919.53l-3.225-.864c-.4-.107-.637-.518-.53-.918l.875-3.241c.107-.4.518-.638.918-.531.4.108.638.518.531.919l-.478 1.769c1.83-1.711 4.272-2.708 6.839-2.708 5.865 0 10.002 4.83 10.002 9.995 0 .724-.081 1.356-.164 1.8z";
+const off =
+  "m2.179 10.201c.055-.298.393-.734.934-.59.377.102.612.476.543.86-.077.529-.141.853-.141 1.529 0 4.47 3.601 8.495 8.502 8.495 2.173 0 4.241-.84 5.792-2.284l-1.251-.341c-.399-.107-.636-.519-.53-.919.108-.4.52-.637.919-.53l3.225.864c.399.108.637.519.53.919l-.875 3.241c-.107.399-.519.636-.919.53-.399-.107-.638-.518-.53-.918l.477-1.77c-1.829 1.711-4.27 2.708-6.838 2.708-5.849 0-9.968-4.8-10.002-9.93-.003-.473.027-1.119.164-1.864zm19.672 3.6c-.054.298-.392.734-.933.59-.378-.102-.614-.476-.543-.86.068-.48.139-.848.139-1.53 0-4.479-3.609-8.495-8.5-8.495-2.173 0-4.241.841-5.794 2.285l1.251.341c.4.107.638.518.531.918-.108.4-.519.637-.919.53l-3.225-.864c-.4-.107-.637-.518-.53-.918l.875-3.241c.107-.4.518-.638.918-.531.4.108.638.518.531.919l-.478 1.769c1.83-1.711 4.272-2.708 6.839-2.708 5.865 0 10.002 4.83 10.002 9.995 0 .724-.081 1.356-.164 1.8z";
+
+window.addEventListener("yt-navigate-start", () => {
+  ytutils.loop = false;
+  document
+    .querySelector("#ytutils-loop")!
+    .querySelector("path")!
+    .setAttribute("d", off);
+});
 
 window.onload = () => {
   const id = setInterval(() => {
@@ -145,6 +154,31 @@ window.onload = () => {
     }
   }, 25);
   if (document.location.pathname !== "/watch") clearInterval(id);
+  const zombie = document.querySelector("#ytutils-loop");
+  if (zombie) zombie.remove();
+  const btn = document.createElement("button");
+  btn.id = "ytutils-loop";
+  btn.className = "ytp-button";
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", off);
+  btn.onclick = () => {
+    ytutils.loop = !ytutils.loop;
+    path.setAttribute("d", ytutils.loop ? on : off);
+    ytutils.player.setLoopVideo(ytutils.loop);
+  };
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewbox", "0 0 24 24");
+  svg.setAttribute("width", "100%");
+  svg.setAttribute("height", "100%");
+  svg.style.fill = "#e6e6e6";
+  svg.style.position = "relative";
+  svg.style.top = "1rem";
+  svg.style.left = "1rem";
+  svg.append(path);
+  btn.append(svg);
+  document
+    .querySelector(".ytp-right-controls")!
+    .insertBefore(btn, document.querySelector(".ytp-subtitles-button"));
 };
 
 window.addEventListener("keydown", (ev) => {
@@ -157,5 +191,8 @@ window.addEventListener("keydown", (ev) => {
         : collapse)!.click();
       break;
     }
+    case "r":
+      document.querySelector<HTMLElement>("#ytutils-loop")!.click();
+      break;
   }
 });
