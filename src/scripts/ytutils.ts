ea9d20649961846ccd82a5d08fc2766eabf0ecae
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-10 12:27:32 -0800

    Created ytutils.setPlayer

--- src/scripts/ytutils.ts
+++ src/scripts/ytutils.ts
@@ -27,6 +27,45 @@ const ytutils = {
   volumelevel: 100,
   loop: false,
   player: document.querySelector<player>("#movie_player")!,
+  setPlayer: () => {
+    const id = setInterval(() => {
+      const player = document.querySelector<player>("#movie_player");
+      if (player) {
+        ytutils.player = player;
+        ytutils.updatePlayer();
+        ytutils.player.addEventListener("onStateChange", (state: number) => {
+          if (state === -1) ytutils.updatePlayer();
+        });
+        clearInterval(id);
+      }
+    }, 25);
+    if (document.location.pathname !== "/watch") clearInterval(id);
+    const zombie = document.querySelector("#ytutils-loop");
+    if (zombie) zombie.remove();
+    const btn = document.createElement("button");
+    btn.id = "ytutils-loop";
+    btn.className = "ytp-button";
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", off);
+    btn.onclick = () => {
+      ytutils.loop = !ytutils.loop;
+      path.setAttribute("d", ytutils.loop ? on : off);
+      ytutils.player.setLoopVideo(ytutils.loop);
+    };
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewbox", "0 0 24 24");
+    svg.setAttribute("width", "100%");
+    svg.setAttribute("height", "100%");
+    svg.style.fill = "#e6e6e6";
+    svg.style.position = "relative";
+    svg.style.top = "1rem";
+    svg.style.left = "1rem";
+    svg.append(path);
+    btn.append(svg);
+    document
+      .querySelector(".ytp-right-controls")!
+      .insertBefore(btn, document.querySelector(".ytp-subtitles-button"));
+  },
   updatePlayer: () => {
     ytutils.setQuality();
     ytutils.setSize();
@@ -133,7 +172,10 @@ const on =
 const off =
   "m2.179 10.201c.055-.298.393-.734.934-.59.377.102.612.476.543.86-.077.529-.141.853-.141 1.529 0 4.47 3.601 8.495 8.502 8.495 2.173 0 4.241-.84 5.792-2.284l-1.251-.341c-.399-.107-.636-.519-.53-.919.108-.4.52-.637.919-.53l3.225.864c.399.108.637.519.53.919l-.875 3.241c-.107.399-.519.636-.919.53-.399-.107-.638-.518-.53-.918l.477-1.77c-1.829 1.711-4.27 2.708-6.838 2.708-5.849 0-9.968-4.8-10.002-9.93-.003-.473.027-1.119.164-1.864zm19.672 3.6c-.054.298-.392.734-.933.59-.378-.102-.614-.476-.543-.86.068-.48.139-.848.139-1.53 0-4.479-3.609-8.495-8.5-8.495-2.173 0-4.241.841-5.794 2.285l1.251.341c.4.107.638.518.531.918-.108.4-.519.637-.919.53l-3.225-.864c-.4-.107-.637-.518-.53-.918l.875-3.241c.107-.4.518-.638.918-.531.4.108.638.518.531.919l-.478 1.769c1.83-1.711 4.272-2.708 6.839-2.708 5.865 0 10.002 4.83 10.002 9.995 0 .724-.081 1.356-.164 1.8z";
 
+window.addEventListener("load", ytutils.setPlayer);
+
 window.addEventListener("yt-navigate-finish", () => {
+  if (!ytutils.player) ytutils.setPlayer();
   ytutils.updatePlayer();
   ytutils.loop = false;
   document
@@ -141,46 +183,6 @@ window.addEventListener("yt-navigate-finish", () => {
     .querySelector("path")!
     .setAttribute("d", off);
 });
-
-window.onload = () => {
-  const id = setInterval(() => {
-    const player = document.querySelector<player>("#movie_player");
-    if (player) {
-      ytutils.player = player;
-      ytutils.updatePlayer();
-      ytutils.player.addEventListener("onStateChange", (state: number) => {
-        if (state === -1) ytutils.updatePlayer();
-      });
-      clearInterval(id);
-    }
-  }, 25);
-  if (document.location.pathname !== "/watch") clearInterval(id);
-  const zombie = document.querySelector("#ytutils-loop");
-  if (zombie) zombie.remove();
-  const btn = document.createElement("button");
-  btn.id = "ytutils-loop";
-  btn.className = "ytp-button";
-  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute("d", off);
-  btn.onclick = () => {
-    ytutils.loop = !ytutils.loop;
-    path.setAttribute("d", ytutils.loop ? on : off);
-    ytutils.player.setLoopVideo(ytutils.loop);
-  };
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("viewbox", "0 0 24 24");
-  svg.setAttribute("width", "100%");
-  svg.setAttribute("height", "100%");
-  svg.style.fill = "#e6e6e6";
-  svg.style.position = "relative";
-  svg.style.top = "1rem";
-  svg.style.left = "1rem";
-  svg.append(path);
-  btn.append(svg);
-  document
-    .querySelector(".ytp-right-controls")!
-    .insertBefore(btn, document.querySelector(".ytp-subtitles-button"));
-};
 
 window.addEventListener("keydown", (ev) => {
   switch (ev.key) {
