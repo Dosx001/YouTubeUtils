9311d18729a7c0adeb681200fcdee87a052ae5a5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-12-05 16:51:23 -0800

    feat(setting): made playback speed a input number/range. closes #1

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,15 +1,17 @@
-const [quality, size, speed] = document.querySelectorAll("select")!;
+const [quality, size] = document.querySelectorAll("select")!;
+const speedNum = document.querySelector<HTMLInputElement>("#speed_num")!;
+const speedRange = document.querySelector<HTMLInputElement>("#speed_range")!;
 const style = document.querySelector<HTMLSelectElement>("#style")!;
 const subtitles = document.querySelector<HTMLSelectElement>("#subtitles")!;
 const level = document.querySelector<HTMLInputElement>("#level")!;
 const play = document.querySelector<HTMLInputElement>("#play")!;
 
-const updateSettings = async () => {
+const updateSettings = () => {
   browser.storage.sync.set({
     play: play.checked,
     quality: quality.options[quality.selectedIndex].value,
     size: size.options[size.selectedIndex].value,
-    speed: Number(speed.options[speed.selectedIndex].value),
+    speed: Number(speedNum.value),
     style: style.value,
     subtitles: subtitles.value,
     volume: document.querySelector<HTMLInputElement>(
@@ -26,15 +28,24 @@ level.onfocus = () =>
 play.onchange = updateSettings;
 quality.onchange = updateSettings;
 size.onchange = updateSettings;
-speed.onchange = updateSettings;
 style.onchange = updateSettings;
 subtitles.onchange = updateSettings;
+
+speedNum.onchange = () => {
+  speedRange.value = speedNum.value;
+  updateSettings();
+};
+speedRange.oninput = () => {
+  speedNum.value = speedRange.value;
+  updateSettings();
+};
 
 browser.storage.sync.get((data: settings) => {
   play.checked = data.play;
   quality.value = data.quality;
   size.value = data.size;
-  speed.value = data.speed.toString();
+  speedNum.value = data.speed.toString();
+  speedRange.value = data.speed.toString();
   style.value = data.style;
   subtitles.value = data.subtitles;
   level.value = data.volumelevel.toString();
